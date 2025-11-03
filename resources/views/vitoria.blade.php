5c536edd72969c725468ea9515453abf93e651a7
PrentysGSAssis <assis.prentys@gmail.com>
2025-11-03 19:59:26 -0300

    03/11/2025 Teste2

---
+++ resources/views/vitoria.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Vitoria Leles</h1>
+    </header>
+    <main>
+        <p>Simplesmente um teste geral para verificar o funcionamento de uma branch</p>
+    </main>
+
+</body>
+</html>
