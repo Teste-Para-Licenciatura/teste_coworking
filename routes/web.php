8d7be47f47afc558ae336477fce645169c47e186
PrentysGSAssis <assis.prentys@gmail.com>
2025-11-04 09:12:28 -0300

    done

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Matheus";
+    $idade = 29;
+
+    return view('welcome', ['nome' => $nome, 'idade' => $idade]);
 });
